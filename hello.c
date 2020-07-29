9d192b76b190a2c1c3d81959d760bd1576ecdf97
khoabin-star <68915143+khoabin-star@users.noreply.github.com>
2020-07-29 11:47:26 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Khoa Tran
+ *         ktran18@cse.unl.edu
+ * Date: 2020/07/28
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Khoa Tran\n");
+  printf("Computer Science and Mathematics\n");
+
+  return 0;
+}
